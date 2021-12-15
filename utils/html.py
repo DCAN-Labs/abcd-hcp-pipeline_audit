e9031b43a6e708e77d4e02258139eca59bf48f0d
Timothy Hendrickson <hendr522@umn.edu>
2021-12-15 11:35:08 -0600

    Revert "Sortable"

--- utils/html.py
+++ utils/html.py
@@ -50,16 +50,21 @@ def html_report_wf(session_statuses_df,report_output_dir,base_dir):
     study_ses_count = len(session_statuses_df)
     pd.set_option('display.max_rows', study_ses_count)
     html = session_statuses_df.copy()
-    html.reset_index(drop=True,inplace=True)
-    #Set table's class="sortable" so that the script works on it
+    html.reset_index(drop=True,inplace=True)    
     htmlstyled = html.style.\
                 applymap(colormap).\
                 set_properties(**{'font-family':'Helvetica','font-size':'8pt','text-align':'center'}).\
                 set_properties(subset=['note'], **{'text-align': 'left'}).\
                 set_table_styles([{'selector': ' ', 'props': [('font-family', 'Helvetica')]}]).\
-                set_table_attributes('class="sortable"').\
                 render()
-    # Added string to the end of htmlstyled calling the JavaScript
-    htmlstyled += '\n' + f"<script src='{os.path.abspath(os.path.join(base_dir, 'scripts','sorttable.min.js'))}'></script>"
+    
     with open(os.path.join(report_output_dir,"s3_status_report.html"),"w") as fp:
         fp.write(htmlstyled)
+    # TODO: currently not generating a sortable HTML report, will need to be tested further
+    """
+    make_sortable_cmd =  base_dir+'/scripts/makesortable.sh ' + report_output_dir + ' ' + base_dir
+    print(make_sortable_cmd)
+    run(make_sortable_cmd)
+    """
+    # print output to stdout
+    #print(session_statuses.to_string())
